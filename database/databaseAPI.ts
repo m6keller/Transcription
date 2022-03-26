20e81eeadd987447b422def475519a879d6504c8
m6keller <kellermatthewe@gmail.com>
2022-03-26 15:36:50 -0400

    handles post and get requests

--- database/databaseAPI.ts
+++ database/databaseAPI.ts
@@ -10,31 +10,30 @@ type Instructions = {
 
 let instructions: Instructions[]; // add all instructinos to this array
 
-class PostsController {
-    public path = '/posts';
-    public router = express.Router();
+// class PostsController {
+//     public path = '/posts';
+//     public router = express.Router();
     
-    private prvInstructions: Instructions[] = instructions; // add all instructinos to this array
+//     private prvInstructions: Instructions[] = instructions; // add all instructinos to this array
 
-    constructor() {
-        this.intializeRoutes();
-    }
-     
-    public intializeRoutes() {
-        this.router.get(this.path, this.getAllPosts);
-        this.router.post(this.path, this.createAPost);
-    }    
+//     constructor() {
+//         this.intializeRoutes();
+//     }
+//     public intializeRoutes() {
+//         this.router.get(this.path, this.getAllPosts);
+//         this.router.post(this.path, this.createAPost);
+//     }    
 
-    getAllPosts = (req: express.Request, res: express.Response) => {
-        res.send(this.prvInstructions);
-    }
+//     getAllPosts = (req: express.Request, res: express.Response) => {
+//         res.send(this.prvInstructions);
+//     }
  
-    createAPost = (req: express.Request, res: express.Response) => {
-        const post: Instructions = req.body;
-        this.prvInstructions.push(post);
-        res.send(post);
-    }
-}
+//     public createAPost = (req: express.Request, res: express.Response) => {
+//         const post: Instructions = req.body;
+//         this.prvInstructions.push(post);
+//         res.send(post);
+//     }
+// }
 
 const router = express.Router();
 const app = express();
@@ -45,6 +44,18 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
+
+app.get('/:id', (req, res) => {
+    const id: number = req.body;
+    if( !id ) res.status(418).send({message: "Invalid id"});
+    res.send(instructions[id]); 
+})
+
+app.post('/:id', (req, res) => {
+    const post: Instructions = req.body;
+    instructions.push(post);
+    res.send(post);
+})
 
 const port = process.env.PORT || 3000;
 
