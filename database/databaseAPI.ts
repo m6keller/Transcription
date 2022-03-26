a01b8dce2c48a5e9a34c8794e0efaa7dec18b389
m6keller <kellermatthewe@gmail.com>
2022-03-26 17:40:15 -0400

    get requests are functional

--- database/databaseAPI.ts
+++ database/databaseAPI.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+const Datastore = require('nedb');
+const instructionDatabase = new Datastore('instructions.db');
+
+instructionDatabase.loadDatabase();
 //import {InstructionMap} from 'hashTable';
 
 type ValuePerUnitOptions = {
@@ -8,22 +12,97 @@ type ValuePerUnitOptions = {
 
 type RefillOptions = {
     needed: boolean;
-    when: number;
+    when: ValuePerUnitOptions;
 }
 
 type Instructions = {
+    patientName: string,
     drugName: string,
     options: ValuePerUnitOptions,
     consumptionInstructions: string,
     frequency: ValuePerUnitOptions,
     periodOfConsuption: ValuePerUnitOptions,
-    timeOfDay: string;
-    instructions: string;
-    expiryDate: number;
-    refillNeeded: RefillOptions
+    timeOfDay: string,
+    instructions: string,
+    expiryDate: number,
+    refillNeeded: RefillOptions,
+    timestamp: number
 }
 
-let instructions: Instructions[]; // add all instructinos to this array
+//var instructions: Instructions[] = new Array( 300 ); // add all instructinos to this array
+
+let temp = JSON.stringify({
+    "patientName": "valma",
+    "drugName": "your_love",
+    "options": {
+        "amount": 50,
+        "units": "mg"
+    },
+    "consumptionInstructions": "eat",
+    "frequency": {
+        "amount": 70,
+        "units": "daily"
+    },
+    "periodOfConsuption": {
+        "amount": 20,
+        "units": "next month"
+    },
+    "timeOfDay": "morning",
+    "instructions": "eat it",
+    "expiryDate": 28400803,
+    "refillNeeded": {
+        "needed": true,
+        "when": {
+            "amount": 40,
+            "units": "days"
+        }
+    }
+});
+
+instructionDatabase.insert(JSON.parse(temp));
+
+const router = express.Router();
+const app = express();
+app.use('/', router);
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('This an API for PharmaHacks 2022 Submission');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This an API for PharmaHacks 2022 Submission');
+});
+
+app.get('/patientName/:id', (req, res) => { // depends what request is
+    const { id } = req.params;
+    instructionDatabase.find({"patientName": id }, (err: any, data: any) => {
+        if( err || !id || data === [] ) res.status(418).send({message: "Invalid id"});
+        else res.send( data );
+    });
+});
+
+app.get('/all', (req, res) => {
+    instructionDatabase.find({ $not:  {patientName: "" } }, (err: any, data: any) => {
+        if( err ) res.status(418).send({message: "Invalid id"});
+        res.send(data);
+    });
+});
+
+app.post('/:id', (req, res) => {
+    const post = req.body;
+    const timestamp: number = Date.now();
+    post.timestamp = timestamp;
+    instructionDatabase.insert(post);
+    res.send(post);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('App listening on PORT ' + port));
+
+
 
 // class PostsController {
 //     public path = '/posts';
@@ -48,30 +127,4 @@ let instructions: Instructions[]; // add all instructinos to this array
 //         this.prvInstructions.push(post);
 //         res.send(post);
 //     }
-// }
-
-const router = express.Router();
-const app = express();
-app.use('/', router);
-app.use(express.json());
-
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
-
-app.get('/:id', (req, res) => {
-    const id: number = req.body;
-    if( !id ) res.status(418).send({message: "Invalid id"});
-    res.send(instructions[id]); 
-})
-
-app.post('/:id', (req, res) => {
-    const post: Instructions = req.body;
-    instructions.push(post);
-    res.send(post);
-})
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log('App listening on PORT ' + port));
+// 
